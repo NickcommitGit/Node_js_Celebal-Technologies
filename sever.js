fd87635afa79da6f2a8aa26117ba0f720aefde1b
Nikhil Mishra <132202589+NickcommitGit@users.noreply.github.com>
2024-07-19 22:59:16 +0530

    Add files via upload

---
+++ sever.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const users = [];
+
+// Registration route
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users.push({ username, password: hashedPassword });
+  res.status(201).send({ message: 'User registered successfully' });
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username);
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ username: user.username }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.status(401).send({ message: 'Invalid credentials' });
+  }
+});
+
+// JWT authentication middleware
+const authenticateJWT = (req, res, next) => {
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+  if (token) {
+    jwt.verify(token, 'your_jwt_secret', (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// Protected route
+app.get('/protected', authenticateJWT, (req, res) => {
+  res.send('This is a protected route');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
